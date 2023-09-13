360ed4ff6a91c561a8a394325117c68275d205d6
saad1551 <saad.ashraf.9094@gmail.com>
2023-09-14 02:14:26 +0500

    ADD: some initial models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
